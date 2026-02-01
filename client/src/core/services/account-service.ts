ea2015b290e043b4caf207209781fc1d5d404c32
snehav <snehavreddy12@gmail.com>
2026-02-01 18:28:52 +0530

    Fixing Button issue, complete register form

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { User } from '../../types/user';
+import { RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
@@ -12,15 +12,29 @@ export class AccountService {
 
   baseUrl = "https://localhost:5001/api/";
 
+  register(creds: RegisterCreds){
+    return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
+      tap(user => {
+        if(user){
+           this.setCurrentUser(user);
+        }
+      })
+    )
+  }
+
   login(creds: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
       tap(user => {
         if(user){
-          localStorage.setItem('user', JSON.stringify(user)); 
-          this.currentUser.set(user)
+          this.setCurrentUser(user);
         }
       })
     )
+  }
+
+  setCurrentUser(user: User){
+    localStorage.setItem('user', JSON.stringify(user)); 
+    this.currentUser.set(user);
   }
 
   logout(){
